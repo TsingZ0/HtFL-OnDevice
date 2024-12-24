19ddbeecbe16311f93c2a2ccb6277c581dc931b5
Tsing <2719584131@qq.com>
2024-12-24 16:06:05 +0800

    update structure

--- system/clients/clientBase.py
+++ system/clients/clientBase.py
@@ -1,59 +1,22 @@
-import argparse
-from collections import OrderedDict
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 import flwr as fl
 from torch.utils.data import DataLoader
 
 
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-transform = transforms.Compose(
-    [
-        transforms.ToTensor(), 
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ]
-)
-
-# Define the local model
-class Net(nn.Module):
-    def __init__(self) -> None:
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc = nn.Linear(84, 10)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc(x)
-        return x
-
-class Client(fl.client.NumPyClient):
+class ClientBase(fl.client.NumPyClient):
     def __init__(self, args, net):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.args = args
-        self.net = net
+        self.net = net.to(self.device)
         self.load_data()
 
     def get_parameters(self, config):
-        head = self.net.fc
-        return [val.cpu().numpy() for _, val in head.state_dict().items()]
+        raise NotImplementedError
 
     def set_parameters(self, parameters):
-        head = self.net.fc
-        params_dict = zip(head.state_dict().keys(), parameters)
-        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
-        head.load_state_dict(state_dict, strict=True)
+        raise NotImplementedError
 
     def fit(self, parameters, config):
         self.set_parameters(parameters)
@@ -68,6 +31,12 @@ class Client(fl.client.NumPyClient):
     # rewite this code to use already assigned local data
     def load_data(self):
         """Load training and test set."""
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(), 
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ]
+        )
         trainset = torchvision.datasets.CIFAR10(
             "test_data", train=True, download=True, transform=transform)
         testset = torchvision.datasets.CIFAR10("test_data", train=False, download=True, transform=transform)
@@ -86,7 +55,7 @@ class Client(fl.client.NumPyClient):
         )
         for _ in range(self.args.epochs):
             for images, labels in self.trainloader:
-                images, labels = images.to(DEVICE), labels.to(DEVICE)
+                images, labels = images.to(self.device), labels.to(self.device)
                 optimizer.zero_grad()
                 loss = criterion(self.net(images), labels)
                 loss.backward()
@@ -98,7 +67,7 @@ class Client(fl.client.NumPyClient):
         correct, total, loss = 0, 0, 0.0
         with torch.no_grad():
             for data in self.testloader:
-                images, labels = data[0].to(DEVICE), data[1].to(DEVICE)
+                images, labels = data[0].to(self.device), data[1].to(self.device)
                 outputs = self.net(images)
                 loss += (criterion(outputs, labels)).sum().item()
                 _, predicted = torch.max(outputs.data, 1)
@@ -107,26 +76,4 @@ class Client(fl.client.NumPyClient):
         loss = loss / total
         accuracy = correct / total
         return loss, accuracy
-
-
-if __name__ == "__main__":
-    # Configration of the client
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--client_id", type=int, default=0)
-    parser.add_argument("--epochs", type=int, default=1)
-    parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--learning_rate", type=float, default=0.001)
-    parser.add_argument("--momentum", type=float, default=0.9)
-    parser.add_argument("--server_address", type=str, default="127.0.0.1:8080")
-    args = parser.parse_args()
-
-    # Load model and data
-    # net = torchvision.models.resnet18(pretrained=False, num_classes=10).to(DEVICE)
-    net = Net().to(DEVICE)
-
-    # Start client
-    fl.client.start_client(
-        server_address=args.server_address, 
-        client=Client(args, net).to_client()
-    )
 
---
+++ system/clients/clientLG.py
@@ -0,0 +1,45 @@
+import argparse
+import torch
+import flwr as fl
+from collections import OrderedDict
+from clientBase import ClientBase
+from utils.models import get_model
+
+
+class Client(ClientBase):
+    def __init__(self, args, net):
+        super().__init__(args, net)
+
+    def get_parameters(self, config):
+        head = self.net.fc
+        return [val.cpu().numpy() for _, val in head.state_dict().items()]
+
+    def set_parameters(self, parameters):
+        head = self.net.fc
+        params_dict = zip(head.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        head.load_state_dict(state_dict, strict=True)
+
+
+if __name__ == "__main__":
+    # Configuration of the client
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--client_id", type=int, default=0)
+    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--learning_rate", type=float, default=0.001)
+    parser.add_argument("--momentum", type=float, default=0.9)
+    parser.add_argument("--model", type=str, default="")
+    parser.add_argument("--num_classes", type=int, default=10)
+    parser.add_argument("--pretrained", type=bool, default=False)
+    parser.add_argument("--server_address", type=str, default="127.0.0.1:8080")
+    args = parser.parse_args()
+
+    # Load model
+    net = get_model(args)
+
+    # Start client
+    fl.client.start_client(
+        server_address=args.server_address, 
+        client=Client(args, net).to_client()
+    )
