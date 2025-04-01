9aff670d866520689ad57b0b636f7533aa576545
Nandinski <amandio.faustino@kaust.edu.sa>
2025-04-01 12:11:56 +0300

    Add script to update CoLExT to main

--- update_colext_to_main.sh
+++ update_colext_to_main.sh
@@ -1,4 +1,18 @@
-#!/bin/bash
+#!/bin/bash -l
 
+set -e
 mamba activate htfl-ondevice
-pip install --force-reinstall git+https://git@github.com/sands-lab/colext.git
+
+INSTALL_DIR=$(pip show colext | awk '/^Location/ {print $2}')
+VERSION=$(pip show colext | awk '/^Version/ {print $2}')
+DIRECT_URL_FILE="$INSTALL_DIR/colext-$VERSION.dist-info/direct_url.json"
+
+INSTALLED_COMMIT=$(jq -r .vcs_info.commit_id "$DIRECT_URL_FILE")
+LATEST_COMMIT=$(git ls-remote $(jq -r .url "$DIRECT_URL_FILE") HEAD | awk '{print $1}')
+if [ "$INSTALLED_COMMIT" != "$LATEST_COMMIT" ]; then
+    echo "CoLExT commit hash does not match latest version."
+    echo "Updating CoLExT"
+    pip install --force-reinstall git+https://git@github.com/sands-lab/colext.git
+else
+    echo "CoLExT is up to date with latest version."
+fi
