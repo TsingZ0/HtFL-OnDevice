6bbb2bb6e80bd3bbc78edaa666601c277542e28f
Nandinski <amandio.faustino@kaust.edu.sa>
2025-08-27 11:01:36 +0300

    colext_benchmarks: Clean up run_benchmark.sh

--- colext_experiments/benchmark/run_benchmark.sh
+++ colext_experiments/benchmark/run_benchmark.sh
@@ -13,30 +13,35 @@ fi
 benchmark_folder=$(basename $1)
 echo "Running benchmark for folder at $benchmark_folder"
 mkdir -p "$benchmark_folder/output"
+mkdir "$benchmark_folder/output/logs"
 
 echo "Creating CoLExT configs using gen_configs.py for this benchmark"
 python3 $benchmark_folder/gen_configs.py
 
 # As jobs finish, print job ids as a map between job id and name. Eg. 1223=ResNet152
-job_id_maps_file="$benchmark_folder/output/output_job_id_maps_`date +%Y%m%d%H%M%S`.txt"
+job_id_maps_file="$benchmark_folder/output/output_job_id_maps_`date +%Y_%m_%d-%H_%M_%S`.txt"
 echo "Prepared output file for job id maps '$job_id_maps_file'"
+echo
 
 for config in "$benchmark_folder"/output/colext_configs/*; do
-    echo "Launching job based on config at: $config"
-    # colext_launch_job -c $config
-    colext_launch_job -c $config > colext_launch_job.output 2>&1
-    echo "Job finished"
-    echo
+    config_id=$(basename "$config" .yaml)
+    log_file="$benchmark_folder/output/logs/${config_id}.log"
+    err_file="$benchmark_folder/output/logs/${config_id}.err"
 
-    # colext_launch_job blocks until job is finished
-    # Once the job is finished, pods will still be available to obtain the job_id
+    echo "Launching job based on: $config"
+    echo "Log file: $log_file"
+    colext_launch_job -c $config > "$log_file" 2> "$err_file"
+    echo "Job finished"
+
+    # Get job_id from pod labels
     job_id=$(mk get pod fl-server  -o=jsonpath='{.metadata.labels.colext-job-id}')
     if [ -z $job_id ]; then
         echo "ERROR: Could not find job id in fl-server pod labels. Stopping benchmark."
         exit 1
     fi
     echo "Finished experiment with job id = $job_id"
-    config_id=$(basename "$config" .yaml | cut -d_ -f2-)
+    echo
+
     # Append job id map to output file
     echo "$job_id=$config_id" >> $job_id_maps_file
 done
